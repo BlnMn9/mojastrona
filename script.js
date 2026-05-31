1f99c520b68ef7b311e9e5232b2fe918d0f03e0e
BlnMn9 <nowakgrzegorz640@protonmail.com>
2026-05-31 13:44:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ let zmienione = false;
 function zmienTekst() {
   const tekst = document.getElementById("tekst");
 
+  if (!tekst) {
+    console.log("Nie znaleziono elementu o id 'tekst'");
+    return;
+  }
+
   if (zmienione === false) {
     tekst.innerText = "Kliknąłeś i działa JavaScript 🚀";
     zmienione = true;
